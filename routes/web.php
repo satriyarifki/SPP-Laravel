beb60b8ecdb6c2d9488f7a67bd38ead1438ba797
satriyarp <rifkisatriya@gmail.com>
2022-12-16 07:09:53 +0700

    Add Docker File

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,19 @@ use App\Http\Controllers\NotificationController;
 Route::middleware(['auth', 'verified', 'installed'])->group(function () {
     Route::get('notifications', NotificationController::class)->name('notifications');
 });
+
+use Illuminate\Support\Facades\Artisan;
+Route::get('/config', function () {
+    Artisan::call(
+        'migrate:fresh',
+        [
+            '--force' => true,
+        ]
+    );
+    Artisan::call(
+        'db:seed',
+        [
+            '--force' => true,
+        ],
+    );
+});
